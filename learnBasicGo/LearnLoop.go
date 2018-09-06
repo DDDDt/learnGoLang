a9c7ad3b5bc182c7ceb733863745293a2fac2168
tao.dai <tao.dai@wehotelglobal.com>
2018-09-06 10:53:31 +0800

    goLang 循环语句

--- learnBasicGo/LearnLoop.go
+++ learnBasicGo/LearnLoop.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*循环判断语句*/
 func main() {
 
@@ -9,5 +11,32 @@ func main() {
 	}*/
 
 	/* for 循环*/
+	for i := 0; i < 10; i++ {
+		fmt.Println("i 的值 %d", i)
+	}
+
+	i := 0
+	for i < 10 {
+		fmt.Println("i 的值 %d", i)
+		i++
+	}
+
+	// 这是一个死循环
+	/*for{
+		fmt.Println("一直进来")
+	}*/
+
+	/*嵌套循环*/
+	for i := 0; i < 100; i++ {
+		if i == 88 {
+			continue
+		}
+		for n := 0; n < 100; n++ {
+			fmt.Println("i 的值 %d,n的值 %d", i, n)
+		}
+		if i == 99 {
+			break
+		}
+	}
 
 }
